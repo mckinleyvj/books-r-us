4da8dc134bc2c6d471b76ccd01d3f28fcf9015c2
BW <binuka.witharana@gmail.com>
2021-10-04 19:13:25 +1100

    search function js

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,76 @@ function getBestSellerAPI() {
 }
 
 getBestSellerAPI();
+
+
+var searchContainer = document.getElementById("result-container");
+
+var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+$(function bookSearch() {
+  $("#search-button").on("click", function(event) {
+    event.preventDefault();
+    // console.log("you've clicked");
+
+    let searchQuery = $("#search-input").val();
+    let baseUrl = bookUrl + searchQuery;
+
+    // console.log(baseUrl);
+
+    if(searchQuery !== ""){
+
+      $.ajax({
+        url: baseUrl,
+        method: "GET",
+        dataType: "json",
+
+        beforeSend: function() {
+          $("#loader").show();
+        },
+
+        complete: function(){
+          $("#loader").hide();
+          $(".results-container").show();
+        },
+
+        success: function(res) {
+          console.log(res);
+
+          var searchOutput = "";
+
+          for (var i = 0; i < res.items.length; i++) {
+            searchOutput += `
+            <div>
+            <h4>${res.items[i].volumeInfo.title}</h4>
+            <br>
+            <img src="${res.items[i].volumeInfo.imageLinks.smallThumbnail}">
+            <br>
+            <p>${res.items[i].volumeInfo.authors}</p>
+            <p>${res.items[i].volumeInfo.publishedDate}</p>
+            <p class="ISBN">ISBN-13 No: ${res.items[i].volumeInfo.industryIdentifiers[0].identifier}</p>
+            <p>${res.items[i].volumeInfo.description}</p>
+            <a href="${res.items[i].volumeInfo.previewLink}" type="url">Preview Book</a>
+            </div>
+            <br>
+            `
+          }
+          if(searchOutput !== "") {
+            $("#search-list").html(searchOutput);
+            console.log(searchQuery);
+
+          } else {
+            let noReslts = "There are no matching results for your query. Please try again."
+            $("#search-list").html(noReslts);
+            console.log(searchQuery);
+          }
+        },
+        error: function() {
+          console.log("error");
+        }
+      });
+    }else{
+      console.log("pls enter something");
+    }
+  });
+  
+});
