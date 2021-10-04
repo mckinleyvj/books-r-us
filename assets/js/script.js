1c59cb610e05eb4eac1a2fa698a3d1706a67bfc3
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-10-04 19:22:08 +1100

    create gbooks API and local storage to store search terms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,81 @@
 const bestSellers = document.getElementById("best-seller-container");
+var list = [];
+
+// local storage to parse json as an array
+if (localStorage.getItem("searchItem")) {
+  list = localStorage.getItem("searchItem");
+  list = JSON.parse(list);
+}
+
+// appending the search items
+function searchList() {
+  let storedList = JSON.parse(localStorage.getItem("searchItem"));
+  document.querySelector("#search-history").innerHTML = "";
+  for (let i = 0; i < storedList.length; i++) {
+    document.querySelector("#search-history").append(storedList[i] + " , ");
+  }
+}
+
+if (list.length > 0) {
+  searchList();
+}
+
+$("#search-button").click(function (event) {
+  event.preventDefault();
+
+  var searchTitle = $("#search-input").val();
+  list.push(searchTitle);
+  localStorage.setItem("searchItem", JSON.stringify(list));
+
+  var responseURL =
+    "https://www.googleapis.com/books/v1/volumes?q=" +
+    searchTitle +
+    "&key=AIzaSyCT1bI8iN0RIzSLYEPmZUDiadmozUA0ZGI";
+
+  console.log(responseURL);
+  $.ajax({
+    url: responseURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    $("#search-result").html("");
+    for (var i = 0; i < response.items.length; i++) {
+      var title = response.items[i].volumeInfo.title;
+      console.log(title);
+      var author = response.items[i].volumeInfo.authors;
+      console.log(author);
+      var publishedDate = response.items[i].volumeInfo.publishedDate;
+      var bookImg = response.items[i].volumeInfo.imageLinks.smallThumbnail;
+      var description = response.items[i].volumeInfo.description;
+      var previewB = response.items[i].volumeInfo.previewLink;
+
+      var displayBooks = $("<p>").html(
+        "Title: " +
+          title +
+          "<br>" +
+          "Author: " +
+          author +
+          "<br>" +
+          "Description: " +
+          description +
+          "<br>" +
+          "Published Date: " +
+          publishedDate +
+          "<hr>" +
+          "<br>"
+      );
+      var preview = $("<a>").attr("href", previewB);
+
+      var Img = $("<img>").attr("src", bookImg);
+
+      $("#search-result").append(Img, displayBooks, preview);
+
+      $("#search-result").css("display", "block");
+    }
+    searchList();
+    // $("#search-button").reset();
+  });
+});
 
 function getBestSellerAPI() {
   var NytAPIKey = "n0MXIKtdoTP0TGIXEBn3dResJCtNHSai";
